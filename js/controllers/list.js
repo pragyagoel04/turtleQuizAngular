5759419112933117023c72463b86e60794971992
pragyagoel04 <pragyagoel04@gmail.com>
2016-09-06 09:41:42 -0700

    added discription and search bar

--- js/controllers/list.js
+++ js/controllers/list.js
@@ -11,6 +11,7 @@
             vm.data=turtlesData;
             vm.activeTurtle={};
             vm.changeActiveTurtle= changeActiveTurtle;
+            vm.search="";
 
             function changeActiveTurtle(index){
                 vm.activeTurtle= index;
